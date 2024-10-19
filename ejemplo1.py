8c11833018f629fce90afa581305a52ec03a4bec
jpinaspucp <jpinas@pucp.pe>
2024-10-19 16:25:27 -0500

    Add files via upload

---
+++ ejemplo1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#import numpy as np
+#import matplotlib.pyplot as plt
+import streamlit as st
+
+st.title('Mi primer proyecto') 
+#st.title()
